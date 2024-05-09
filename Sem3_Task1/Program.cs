f8b2a1abafa90533da8d050a27761459a3ae5c28
Raymiray <ritsiscream11@yandex.ru>
2024-05-09 15:11:25 +0300

    add sem3:task1

---
+++ Sem3_Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в
+// массиве. Программа должна выдать ответ: Да/Нет.
+// Примеры
+// [1 3 4 19 3], 8 => Нет
+// [-4 3 4 1], 3 => Да
+
+// int[] array1 = new int[5]; // по умолчанию заполнено нулями
+// int[] array2 = new int[5] { 3, 6, 8, 3, 9 }; // позволяет контролировать количество значений
+// int[] array3 = new int[] { 3, 6, 8, 3, 9 };
+
+int[] array = { 1, 3, 4, 19, 3 };
+int num = 8;
+
+bool isExistNum = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[1]  == num)
+    {
+        isExistNum = true;
+        break;
+    }
+}
+string result = isExistNum ? "Yes" : "No";
+Console.WriteLine(result);
